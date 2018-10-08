391ed6204c30c9cf32c9cac0bceab360912a0dfc
DonVolkRam <success.vka@gmail.com>
2018-10-08 17:11:08 +0300

    Причесывание первого задания

---
+++ Ex2/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Ex2
+{
+    class Program
+    {
+        //Список функций
+        public static double F(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+        public static double Sin(double x)
+        {
+            return Math.Sin(x);
+        }
+        public static double Cos(double x)
+        {
+            return Math.Cos(x);
+        }
+        public static double Tan(double x)
+        {
+            return Math.Tan(x);
+        }
+        public static double Exp(double x)
+        {
+            return Math.Exp(x);
+        }
+        public static double Pow2(double x)
+        {
+            return x*x;
+        }
+        public static double Pow3(double x)
+        {
+            return Math.Pow(x,3);
+        }
+        public static double PowDev2(double x)
+        {
+            return 1 / x;
+        }
+        public static double XSin(double x)
+        {
+            return Math.Sin(x)*x;
+        }
+        public static double XCos(double x)
+        {
+            return Math.Cos(x) * x;
+        }
+        public static double ExpCos(double x)
+        {
+            return Math.Cos(x) * Math.Exp(x);
+        }
+        public static double ExpSin(double x)
+        {
+            return Math.Sin(x) * Math.Exp(x);
+        }
+
+        public delegate double Function(double x);
+
+        List<Function> DelegaList;
+
+//        public List<Function> DelegaList1 { get => DelegaList; set => DelegaList = value; }
+
+        /// <summary>
+        /// Процедура инициализация коллекции делегатов с функциями
+        /// </summary>
+        /// <param name="DelegaList">Коллекция делегатов которую нужно инициализировать</param>
+        public static void InitDelegate(out List<Function> DelegaList)
+        {
+            DelegaList = new List<Function>();
+            DelegaList.Add(F);
+            DelegaList.Add(Sin);
+            DelegaList.Add(Cos);
+            DelegaList.Add(Tan);
+            DelegaList.Add(Exp);
+            DelegaList.Add(Pow2);
+            DelegaList.Add(Pow3);
+            DelegaList.Add(PowDev2);
+            DelegaList.Add(XSin);
+            DelegaList.Add(XCos);
+            DelegaList.Add(ExpCos);
+            DelegaList.Add(ExpSin);
+        }
+
+        /// <summary>
+        /// Процедура сохранения значений функции выбранной из списка функций
+        /// </summary>
+        /// <param name="fileName">Имя файла куда необходима записать данные</param>
+        /// <param name="F">Функция по которой будет производится расчет</param>
+        /// <param name="a">Нижняя граница интервалла</param>
+        /// <param name="b">Верхняя граница интервалла</param>
+        /// <param name="h">Шаг интервалла</param>
+        public static void SaveFunc(string fileName, Function F, double a, double b, double h)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create,
+            FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(F(x));
+                x += h; // x=x+h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+
+        /// <summary>
+        /// Функция загрузки из файла минимального значения 
+        /// </summary>
+        /// <param name="fileName">Имя файла где хранятся данные функции</param>
+        /// <param name="min">Передаваемый параментр, которому будет присвоено минимальное значение из массива данных в файле</param>
+        /// <returns></returns>
+        public static double[] Load(string fileName, ref double min)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            min = double.MaxValue;
+            double[] d = new double[fs.Length / sizeof(double)];
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d[i] = bw.ReadDouble();
+                if (d[i] < min) min = d[i];
+            }
+            bw.Close();
+            fs.Close();
+            return d;
+        }
+
+
+        public static void Main(string[] args)
+        {
+            List<Function> DelegaList;
+            InitDelegate(out DelegaList);
+            string Filename = "data.bin";
+            double Minimum = 0;
+            int choise = 0;
+            int min=0, max=0;
+            double step = 0;
+            Console.WriteLine("Выбирете функцию и введите ее порядковый номер\n" +
+            "1) F = x * x - 50 * x + 10\n" +
+            "2) F = Sin(x)\n" +
+            "3) F = Cos(x)\n" +
+            "4) F = Tan(x)\n" +
+            "5) F = Exp(x)\n" +
+            "6) F = x * х\n" +
+            "7) F = x ^ 3\n" +
+            "8) F = 1 / x\n" +
+            "9) F = x * Sin(x)\n" +
+            "10) F = x * Cos(x)\n" +
+            "11) F = Exp(x) * Cos(x)\n" +
+            "12) F = Exp(x) * Sin(x)\n");
+
+            string temp;
+            try
+            {
+                temp = Console.ReadLine();
+                if ("q" == Convert.ToString(temp))
+                {
+                    Console.WriteLine("До свидания! Всего доброго!");
+                    Console.Read();                   
+                }
+                else
+                    choise = Convert.ToInt32(temp);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный формат ввода. Введите число соответствующее номеру задачи");
+            }
+
+            choise = Console.Read() - 1;
+            Console.WriteLine("Задайте интервал");
+            Console.Write("\nВведите минимальное значение");
+            min = Console.Read();
+            Console.Write("\nВведите максимальное значение");
+            max = Console.Read();
+            Console.Write("\nВведите шаг");
+            step = double.Parse(Console.ReadLine());
+            SaveFunc(Filename, DelegaList[choise], min, max, step);
+
+//            int Lenght = Load(Filename, ref Minimum);
+            double[] Array = Load(Filename, ref Minimum);            
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.Write( "  {0,8:0.000}", Array[i]);
+                if (i!=0 && i%5==0)
+                {
+                    Console.WriteLine("\n");
+                }
+            }
+
+            Console.WriteLine("Минимум равен = " + Minimum);
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ Ex3/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Ex1
+{
+    class Program
+    {
+
+        static int StudName(Student st1, Student st2)
+        {
+            return String.Compare(st1.firstName, st2.firstName); 
+        }
+
+        static int StudAge(Student st1, Student st2) 
+        {
+            return String.Compare(st1.age.ToString(), st2.age.ToString()); 
+        }
+
+        static int StudCourseAge(Student st1, Student st2) 
+        {
+            return String.Compare((100*st1.course + st1.age).ToString(), (100*st2.course + st2.age).ToString());
+        }
+
+        static void Main(string[] args)
+        {
+            int bakalavr = 0;
+            int magistr = 0;
+            int MaxCourses = 7;
+            int[] courses = new int[MaxCourses];
+
+            bool firstRead = true;
+            List<Student> list = new List<Student>();
+            // Создаем список студентов
+            DateTime dt = DateTime.Now;
+            StreamReader sr = new StreamReader("..\\..\\Students.csv");
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+                    if (firstRead)
+                    {
+                        for (int i = 0; i < 3; i++)
+                        {
+                            string[] temp = sr.ReadLine().Split(';');
+                        }
+                        firstRead = false;
+                    }   
+                    string[] s = sr.ReadLine().Split(';');
+                    // Добавляем в список новый экземпляр класса Student
+                    list.Add(new
+                    Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7])
+                    , s[8]));
+                    // Одновременно подсчитываем количество студентов на различных курсах
+                    switch (int.Parse(s[6]))
+                    {
+                        case 1: courses[1]++; break;
+                        case 2: courses[2]++; break;
+                        case 3: courses[3]++; break;
+                        case 4: courses[4]++; break;
+                        case 5: courses[5]++; break;
+                        case 6: courses[6]++; break;
+                        default: break;
+                    }
+
+
+                    if (int.Parse(s[6]) < 5) bakalavr++; else magistr++;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Ошибка!ESC - прекратить выполнение программы");
+                    // Выход из Main
+                    if (Console.ReadKey().Key == ConsoleKey.Escape) return;
+                }
+            }
+            sr.Close();            
+            Console.WriteLine("Всего студентов : " + list.Count);
+            Console.WriteLine("Бакалавров : {0}\n", bakalavr);
+            Console.WriteLine("Магистров : {0}", magistr);
+            
+            for (int i = 1; i < MaxCourses; i++)
+            {
+                Console.WriteLine("Количество стдентов на {0} курсе = {1}", i, courses[i]);
+            }
+
+            //list.Sort(new Comparison<Student>(StudAge));
+            //foreach (var v in list) Console.WriteLine(v.firstName + " " + v.age + " лет ");
+
+            list.Sort(new Comparison<Student>(StudCourseAge));
+            foreach (var v in list) Console.WriteLine(v.firstName + " " + v.course + " Курс " + v.age + " Лет");
+
+            Console.WriteLine(DateTime.Now - dt);
+            Console.ReadKey();
+        }
+    }
+}
